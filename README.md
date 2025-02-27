9f3c4746289f6cb2a242e0360c8ed29a5f437430
maxg56 <mgendrot56@gmail.com>
2025-02-27 19:41:52 +0100

    ba dokeer

---
+++ README.md
@@ -0,0 +1,17 @@
+# Inception
+- istale ```sudo pacman -S docker```
+
+### Dockerfile
+
+- FROM
+- RUN
+- copy
+
+## comende
+
+```sh
+docker build <pas> -t<nem> 
+```
+```sh
+ docker ps -q > l
+ ```
---
+++ bude-ren.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <image_name> <dockerfile_directory>"
+    exit 1
+fi
+
+docker build -t "$1" "$2"
+if [ $? -eq 0 ]; then
+    docker run --rm -p 2345:443 -it "$1" 
+else
+    echo "Docker build failed!"
+fi
