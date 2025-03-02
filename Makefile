a1e563d6ab57a10afbe2efc26df73851473e00c4
maxg56 <mgendrot56@gmail.com>
2025-03-02 23:10:22 +0100

    samarch

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: maxence <maxence@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/02/28 20:57:00 by maxence           #+#    #+#              #
-#    Updated: 2025/02/28 20:57:03 by maxence          ###   ########.fr        #
+#    Updated: 2025/03/02 22:50:44 by maxence          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,12 +17,16 @@ DOCKER_COMPOSE_PATH	=	srcs/docker-compose.yml
 
 all:
 	@if [ -f "./srcs/.env" ]; then \
+		mkdir -p /home/maxence/data/mariadb; \
+		mkdir -p /home/maxence/data/wordpress; \
 		$(DOCKER_COMPOSE_CMD) -p $(NAME) -f $(DOCKER_COMPOSE_PATH) up --build -d; \
 	else \
 		echo "No .env file found in srcs folder, please create one before running make"; \
 	fi
 
 #
+stop:
+	$(DOCKER_COMPOSE_CMD) -p $(NAME) -f $(DOCKER_COMPOSE_PATH) stop
 
 down:
 	$(DOCKER_COMPOSE_CMD) -p $(NAME) -f $(DOCKER_COMPOSE_PATH) down -v
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,79 +1,61 @@
 #!/bin/sh
-
 set -e
 
-# Ensure required environment variables are set
-: "${SQL_DATABASE:?SQL_DATABASE not set}"
-: "${SQL_USER:?SQL_USER not set}"
-: "${SQL_PASSWORD:?SQL_PASSWORD not set}"
-: "${DOMAIN_NAME:?DOMAIN_NAME not set}"
-: "${WP_TITLE:?WP_TITLE not set}"
-: "${WP_ADMIN_USER:?WP_ADMIN_USER not set}"
-: "${WP_ADMIN_PASSWORD:?WP_ADMIN_PASSWORD not set}"
-: "${WP_ADMIN_EMAIL:?WP_ADMIN_EMAIL not set}"
-: "${WP_USER_USER:?WP_USER_USER not set}"
-: "${WP_USER_EMAIL:?WP_USER_EMAIL not set}"
-: "${WP_USER_PASSWORD:?WP_USER_PASSWORD not set}"
-
-# Navigate to the WordPress directory
 cd /var/www/wordpress
 
-# Create wp-config.php if it doesn't exist
+until mysql -h mariadb -u $SQL_USER -p$SQL_PASSWORD -e "SELECT 1" > /dev/null 2>&1; do
+  echo "Waiting for MariaDB to be ready..."
+  sleep 5
+done
+
+
+# Vérifier si WordPress est déjà installé
 if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
-  echo "Creating wp-config.php..."
-  wp config create \
-    --allow-root \
-    --dbname=$SQL_DATABASE \
-    --dbuser=$SQL_USER \
-    --dbpass=$SQL_PASSWORD \
-    --dbhost=mariadb:3306 \
-    --path='/var/www/wordpress' \
-    --locale=fr_FR
-  echo "wp-config.php created"
+    echo "WordPress is not installed yet. Installing WordPress..."
+    wp config create \
+        --allow-root \
+        --dbname=$SQL_DATABASE \
+        --dbuser=$SQL_USER \
+        --dbpass=$SQL_PASSWORD \
+        --dbhost=mariadb:3306 \
+        --path='/var/www/wordpress' \
+        --locale=fr_FR
+else
+    echo "[1]WordPress is already installed."
 fi
 
-# Install WordPress if not already installed
 if ! wp core is-installed --path='/var/www/wordpress'; then
-  echo "Installing WordPress..."
-  wp core install \
-    --url=$DOMAIN_NAME/ \
-    --title=$WP_TITLE \
-    --admin_user=$WP_ADMIN_USER \
-    --admin_password=$WP_ADMIN_PASSWORD \
-    --admin_email=$WP_ADMIN_EMAIL \
-    --skip-email \
-    --allow-root \
-    --path='/var/www/wordpress'
-  echo "WordPress installed"
+    wp core install \
+        --url="$DOMAIN_NAME/" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN_USER" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --skip-email \
+        --allow-root \
+        --path='/var/www/wordpress'
 else
-  echo "WordPress is already installed."
+    echo "[2] WordPress is already installed."
 fi
 
-# Check if user already exists
-if ! wp user get $WP_USER_USER --path='/var/www/wordpress' > /dev/null 2>&1; then
-  # Create the user if it doesn't exist
-  echo "Creating WordPress user..."
-  wp user create $WP_USER_USER $WP_USER_EMAIL \
-    --role=author \
-    --user_pass=$WP_USER_PASSWORD \
-    --allow-root \
-    --path='/var/www/wordpress'
-  echo "User created"
+if ! wp user get "$WP_USER_USER" --path='/var/www/wordpress'; then
+    wp user create "$WP_USER_USER" "$WP_USER_EMAIL" \
+        --role=author \
+        --user_pass="$WP_USER_PASSWORD" \
+        --allow-root \
+        --path='/var/www/wordpress'
 else
-  echo "User $WP_USER_USER already exists."
+    echo "[3] User $WP_USER_USER already exists."
 fi
 
-# Install and activate the Astra theme
-echo "Installing Astra theme..."
-wp theme install astra \
-  --activate \
-  --allow-root \
-  --path='/var/www/wordpress'
-echo "Astra theme activated"
+if ! wp theme is-installed astra --path='/var/www/wordpress'; then
+    wp theme install astra --activate --allow-root --path='/var/www/wordpress'
+else
+    echo "[4] Astra theme is already installed."
+fi
 
-# Ensure the PHP-FPM socket directory exists
+
 mkdir -p /run/php
 
-# Start PHP-FPM
 echo "Starting PHP-FPM..."
 /usr/sbin/php-fpm83 -F
